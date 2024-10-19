e46abad898980af17df76c9249ad2ff1a0e54455
tommyBurgos <trburgos@espol.edu.ec>
2024-10-19 15:08:48 -0500

    Cambios finales subir libros en linea

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls'))
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns+=[
     re_path(r'^media/(?P<path>.*)$',serve,{
         'document_root':settings.MEDIA_ROOT,
